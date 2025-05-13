7bcc38f57fead1399deb9e4b4c2a0928b6927800
Samuel Belarmino Wanderley <samuelbelarminowanderley@gmail.com>
2025-05-13 18:55:37 -0300

    Criação da data e hora

--- projetodjango/app/models.py
+++ projetodjango/app/models.py
@@ -4,6 +4,7 @@ class Tarefa(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField(blank=True) 
     feita = models.BooleanField(default=False)
+    data_hora = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.titulo
--- projetodjango/app/views.py
+++ projetodjango/app/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render,redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarefa
-
+from django.utils.dateparse import parse_datetime
 
 def index(request):
     if request.method == "POST":
         titulo = request.POST.get("titulo", "").strip()
         descricao = request.POST.get("descricao", "").strip()
+        data_hora = request.POST.get("horario")  # Agora usando "horario", que é o nome do campo no HTML
+        horario = parse_datetime(data_hora) if data_hora else None
         if titulo:
-            Tarefa.objects.create(titulo=titulo,descricao=descricao)
+            Tarefa.objects.create(titulo=titulo, descricao=descricao, data_hora=horario)
         return redirect("index")
 
     tarefas = Tarefa.objects.all().order_by("feita")
@@ -18,7 +20,6 @@ def marcar_feita(request, tarefa_id):
     tarefa.feita = not tarefa.feita
     tarefa.save()
     return redirect("index")
-
 
 def excluir_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
